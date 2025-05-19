30e95df6daf5cf9e08989163339341db2f86326f
coderpandu <coderbadshah777@gmail.com>
2025-05-19 19:56:27 +0545

    perimeter

--- area.c
+++ area.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
 int main() {
-    int length, width, area;
+    int length, width, area, perimeter;
     printf("Enter length and width of rectangle: ");
     scanf("%d %d", &length, &width);
     area = length * width;
+    perimeter = 2 * (length + width);
+    printf("Perimeter of rectangle is: %d\n", perimeter);
     printf("Area of rectangle is: %d\n", area);
     return 0;
 }
